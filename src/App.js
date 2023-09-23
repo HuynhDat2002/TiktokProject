be1b47c3bb674e6ae94af222f51183ae0c4b29bd
My Mac <mymac@MacBook-cua-My.local>
2023-09-23 15:51:23 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Fragment } from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Home from '~/pages/Home'
 import Following from '~/pages/Following'
 import {publicRoutes} from '~/routes'
+import {DefaultLayout} from '~/components/Layout'
+
 
 function App() {
     return (
@@ -9,8 +12,27 @@ function App() {
           <div className="App">
             <Routes>
               {publicRoutes.map((route,index )=>{
+                let Layout=DefaultLayout
+                if (route.layout){
+                  Layout=route.layout
+                }
+                else if(route.layout===null){
+                  Layout=Fragment
+                }
                 const Page=route.component
-                return <Route key={index} path={route.path} element={<Page />}/>
+                return (
+                  <Route 
+                      key={index} 
+                      path={route.path} 
+                      element={
+                        <Layout>
+
+                          <Page />
+                        </Layout>
+                      }
+
+                  />
+                );
               })}
 
             </Routes>
